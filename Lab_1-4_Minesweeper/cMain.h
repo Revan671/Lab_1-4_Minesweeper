bb87e8d93f29fbdcf67984dce87f20604021b40c
Eric Wade <ECWade1@student.fullsail.edu>
2022-06-08 20:28:34 -0600

    Created OnButtonClicked Event and the Event Table. Linked m_btn1 top the OnButtonClicked event

--- Lab_1-4_Minesweeper/cMain.h
+++ Lab_1-4_Minesweeper/cMain.h
@@ -11,5 +11,11 @@ public:
 	wxButton *m_btn1 = nullptr;
 	wxTextCtrl *m_txt1 = nullptr;
 	wxListBox *m_lst1 = nullptr;
+
+	void OnButtonClicked(wxCommandEvent& evt);
+
+	wxDECLARE_EVENT_TABLE();
+
+
 };
 
